881830f60d01e2008a0173617cd2c7a04a4a8fec
Dan Moran <danmoran@broadinstitute.org>
2019-02-20 16:27:55 -0500

    It compiles!

--- manager/src/main/scala/org/broadinstitute/transporter/api/ManagerApi.scala
+++ manager/src/main/scala/org/broadinstitute/transporter/api/ManagerApi.scala
@@ -2,26 +2,28 @@ package org.broadinstitute.transporter.api
 
 import cats.effect.IO
 import org.broadinstitute.transporter.ManagerApp
+import org.broadinstitute.transporter.status.ManagerStatus
 import org.http4s._
-import org.http4s.circe.CirceEntityEncoder
 import org.http4s.rho.RhoRoutes
 import org.http4s.rho.bits.PathAST.{PathMatch, TypedPath}
 import org.http4s.rho.swagger.models.Info
-import org.http4s.rho.swagger.syntax.{io => swaggerIo}
+import org.http4s.rho.swagger.SwaggerSupport
 
-class ManagerApi(appVersion: String, apiDocsPath: String, app: ManagerApp) {
+class ManagerApi(appVersion: String, apiDocsPath: String, app: ManagerApp)
+    extends SwaggerSupport[IO] {
 
-  import swaggerIo._
+  private implicit val statusEncoder: EntityEncoder[IO, ManagerStatus] =
+    org.http4s.circe.jsonEncoderOf
 
-  private val infoRoutes = new RhoRoutes[IO] with CirceEntityEncoder {
+  private val infoRoutes = new RhoRoutes[IO] {
 
     // NOTE: Has to be a thunk here to prevent eager evaluation / caching.
     "Query operational status of the system" ** GET / "status" |>> { () =>
-      app.statusController.status.map { status =>
+      app.statusController.status.flatMap { status =>
         if (status.ok) {
-          Ok.pure(status)
+          Ok(status)
         } else {
-          InternalServerError.pure(status)
+          InternalServerError(status)
         }
       }
     }
@@ -32,7 +34,7 @@ class ManagerApi(appVersion: String, apiDocsPath: String, app: ManagerApp) {
   }
 
   def routes: HttpRoutes[IO] = {
-    val swaggerMiddleware = swaggerIo.createRhoMiddleware(
+    val swaggerMiddleware = createRhoMiddleware(
       apiInfo = Info(
         title = "Transporter API",
         version = appVersion,
