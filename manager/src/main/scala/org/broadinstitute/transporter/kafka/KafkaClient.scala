5d62a7d2b7d174ea05e1489a85a6e94f36588122
Dan Moran <danmoran@broadinstitute.org>
2019-02-22 11:09:53 -0500

    Simpler.

--- manager/src/main/scala/org/broadinstitute/transporter/kafka/KafkaClient.scala
+++ manager/src/main/scala/org/broadinstitute/transporter/kafka/KafkaClient.scala
@@ -3,7 +3,6 @@ package org.broadinstitute.transporter.kafka
 import java.util.concurrent.{CancellationException, CompletionException}
 
 import cats.effect._
-import cats.effect.concurrent.Deferred
 import fs2.kafka.AdminClientSettings
 import org.apache.kafka.clients.admin.AdminClient
 import org.apache.kafka.common.KafkaFuture
@@ -12,16 +11,16 @@ import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 
 class KafkaClient private[kafka] (
-  clientPromise: Deferred[IO, AdminClient],
+  adminClient: AdminClient,
   blockingEc: ExecutionContext
 )(implicit cs: ContextShift[IO], t: Timer[IO]) {
 
   import KafkaClient.KafkaFutureSyntax
 
   def checkReady: IO[Boolean] = {
+    val clusterId = IO.suspend(adminClient.describeCluster().clusterId().cancelable)
+
     val okCheck = for {
-      adminClient <- clientPromise.get
-      clusterId = IO.suspend(adminClient.describeCluster().clusterId().cancelable)
       id <- cs.evalOn(blockingEc)(clusterId)
       _ <- IO.delay(println(s"Got $id"))
     } yield {
@@ -66,38 +65,16 @@ object KafkaClient {
       .withClientId(config.clientId)
       .withCloseTimeout(1.second)
 
+    val initClient =
+      cs.evalOn(blockingEc)(settings.adminClientFactory.create[IO](settings))
+
+    val closeClient = (client: AdminClient) =>
+      cs.evalOn(blockingEc)(IO.delay {
+        client.close(settings.closeTimeout.length, settings.closeTimeout.unit)
+      })
+
     Resource
-      .make(initClient(settings, blockingEc)) {
-        case (fiber, promise) => closeClient(fiber, promise, settings.closeTimeout)
-      }
-      .map {
-        case (_, promise) => new KafkaClient(promise, blockingEc)
-      }
+      .make(initClient)(closeClient)
+      .map(new KafkaClient(_, blockingEc))
   }
-
-  private def initClient(settings: AdminClientSettings, blockingEc: ExecutionContext)(
-    implicit cs: ContextShift[IO]
-  ): IO[(Fiber[IO, Unit], Deferred[IO, AdminClient])] =
-    Deferred[IO, AdminClient].flatMap { promise =>
-      val createClient = for {
-        client <- cs.evalOn(blockingEc)(settings.adminClientFactory.create[IO](settings))
-        _ <- promise.complete(client)
-      } yield {
-        ()
-      }
-
-      createClient.start.map(_ -> promise)
-    }
-
-  private def closeClient(
-    initFiber: Fiber[IO, Unit],
-    clientPromise: Deferred[IO, AdminClient],
-    timeout: FiniteDuration
-  )(implicit cs: ContextShift[IO], t: Timer[IO]): IO[Unit] =
-    initFiber.cancel.flatMap { _ =>
-      val tryCancel = clientPromise.get.flatMap(
-        admin => IO.delay(admin.close(timeout.length, timeout.unit))
-      )
-      tryCancel.timeoutTo(timeout, IO.unit)
-    }
 }
