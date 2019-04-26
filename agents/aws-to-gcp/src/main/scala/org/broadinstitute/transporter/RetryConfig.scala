a089d27bb65eb56354869f20783bb6b98d542794
Dan Moran <danmoran@broadinstitute.org>
2019-04-26 17:00:39 -0400

    Add comments.

--- agents/aws-to-gcp/src/main/scala/org/broadinstitute/transporter/RetryConfig.scala
+++ agents/aws-to-gcp/src/main/scala/org/broadinstitute/transporter/RetryConfig.scala
@@ -5,6 +5,12 @@ import pureconfig.generic.semiauto.deriveReader
 
 import scala.concurrent.duration.FiniteDuration
 
+/**
+  * Parameters for tweaking the approach used for retrying transient HTTP failures.
+  *
+  * @param maxRetries upper bound on number of times a single request can be retried
+  * @param maxDelay upper bound on time that can be spent between two retries of a single request
+  */
 case class RetryConfig(maxRetries: Int, maxDelay: FiniteDuration)
 
 object RetryConfig {
