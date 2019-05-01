1119a1546f87bf93b121508033130e61633ecad7
Dan Moran <danmoran@broadinstitute.org>
2019-05-01 11:57:36 -0400

    Scaladocs.

--- common/src/main/scala/org/broadinstitute/transporter/transfer/TransferRequest.scala
+++ common/src/main/scala/org/broadinstitute/transporter/transfer/TransferRequest.scala
@@ -5,6 +5,13 @@ import java.util.UUID
 import io.circe.{Decoder, Encoder}
 import io.circe.derivation.{deriveDecoder, deriveEncoder}
 
+/**
+  * Trigger message for a transfer, sent by the manager to agents via Kafka.
+  *
+  * @param transfer user-provided request body describing the transfer to perform
+  * @param id unique ID for the transfer operation
+  * @param requestId unique ID for the bulk request which includes the transfer
+  */
 case class TransferRequest[R](transfer: R, id: UUID, requestId: UUID)
 
 object TransferRequest {
--- common/src/main/scala/org/broadinstitute/transporter/transfer/TransferSummary.scala
+++ common/src/main/scala/org/broadinstitute/transporter/transfer/TransferSummary.scala
@@ -6,10 +6,13 @@ import io.circe.{Decoder, Encoder}
 import io.circe.derivation.{deriveDecoder, deriveEncoder}
 
 /**
-  * Description of an attempt to run a transfer.
+  * Description of an attempt to run a transfer, sent by agents to the manager via Kafka.
   *
   * @param result signal describing the success/failure of the attempt
-  * @param info optional extra information describing the status signal in more detail
+  * @param info agent-specified payload describing either 1) outputs of the successful transfer
+  *             or 2) details of the error which caused the transfer to fail
+  * @param id unique ID for the transfer operation
+  * @param requestId unique ID for the bulk request which includes the transfer
   */
 case class TransferSummary[I](result: TransferResult, info: I, id: UUID, requestId: UUID)
 
