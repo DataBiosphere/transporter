cb0bcbc6bf06ba06d098500ca67bab58338ea452
Dan Moran <danmoran@broadinstitute.org>
2019-06-03 15:08:19 -0400

    Remove unused DB utils.

--- manager/src/main/scala/org/broadinstitute/transporter/db/DoobieInstances.scala
+++ manager/src/main/scala/org/broadinstitute/transporter/db/DoobieInstances.scala
@@ -5,13 +5,9 @@ import java.time.{OffsetDateTime, ZoneId}
 
 import doobie.postgres.{Instances => PostgresInstances}
 import doobie.postgres.circe.Instances.JsonInstances
-import doobie.util.{Get, Put}
-import org.broadinstitute.transporter.transfer.config.TransferSchema
+import doobie.util.Get
 
 object DoobieInstances extends PostgresInstances with JsonInstances {
-
-  implicit val schemaGet: Get[TransferSchema] = pgDecoderGet
-  implicit val schemaPut: Put[TransferSchema] = pgEncoderPut
 
   implicit val odtGet: Get[OffsetDateTime] = Get[Timestamp].map { ts =>
     OffsetDateTime.ofInstant(ts.toInstant, ZoneId.of("UTC"))
